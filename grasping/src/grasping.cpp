a49ebc342ba06cf333055d69eede7ae42f8668f8
Frank Zillmann <frank.zillmann@tum.de>
2026-06-03 16:00:49 +0200

    add implementation of open,close,carry_pose services (untested)

---
+++ grasping/src/grasping.cpp
@@ -0,0 +1,113 @@
+#include <map>
+#include <memory>
+
+#include <rclcpp/rclcpp.hpp>
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <std_srvs/srv/trigger.hpp>
+
+namespace
+{
+const std::map<std::string, double> carry_pose = {
+  {"shoulder_pan_joint", 0.00022788182014732428},
+  {"shoulder_lift_joint", 0.08524623344398141},
+  {"elbow_joint", -0.05947614807949897},
+  {"wrist_joint", -1.56},
+};
+}
+
+class GraspingServices
+{
+public:
+  using Trigger = std_srvs::srv::Trigger;
+
+  explicit GraspingServices(const rclcpp::Node::SharedPtr & node)
+  : node_(node),
+    arm_interface_(node_, "mirte_arm"),
+    gripper_interface_(node_, "mirte_gripper")
+  {
+    RCLCPP_INFO(node_->get_logger(), "Moveit Arm Planning frame: %s", arm_interface_.getPlanningFrame().c_str());
+    RCLCPP_INFO(node_->get_logger(), "Moveit Gripper Planning frame: %s", gripper_interface_.getPlanningFrame().c_str());
+
+    arm_interface_.setMaxVelocityScalingFactor(0.5);
+    arm_interface_.setMaxAccelerationScalingFactor(0.5);
+    arm_interface_.setPlanningTime(1.0);
+    arm_interface_.setNumPlanningAttempts(3);
+    arm_interface_.setGoalPositionTolerance(0.02);
+    arm_interface_.setGoalOrientationTolerance(3.14159);
+    arm_interface_.setGoalJointTolerance(0.1);
+    open_gripper_service_ = node_->create_service<Trigger>(
+      "open_gripper",
+      [this](const std::shared_ptr<Trigger::Request>, std::shared_ptr<Trigger::Response> response) {
+        move_gripper("open", "opened gripper", "failed to open gripper", response);
+      });
+
+    close_gripper_service_ = node_->create_service<Trigger>(
+      "close_gripper",
+      [this](const std::shared_ptr<Trigger::Request>, std::shared_ptr<Trigger::Response> response) {
+        move_gripper("close", "closed gripper", "failed to close gripper", response);
+      });
+
+    move_to_carry_pose_service_ = node_->create_service<Trigger>(
+      "move_to_carry_pose",
+      [this](const std::shared_ptr<Trigger::Request>, std::shared_ptr<Trigger::Response> response) {
+        move_arm_to_carry_pose(response);
+      });
+  }
+
+private:
+  rclcpp::Node::SharedPtr node_;
+  moveit::planning_interface::MoveGroupInterface arm_interface_;
+  moveit::planning_interface::MoveGroupInterface gripper_interface_;
+  rclcpp::Service<Trigger>::SharedPtr open_gripper_service_;
+  rclcpp::Service<Trigger>::SharedPtr close_gripper_service_;
+  rclcpp::Service<Trigger>::SharedPtr move_to_carry_pose_service_;
+
+  bool move_gripper(
+    const char * target,
+    const char * success_message,
+    const char * failure_message,
+    const std::shared_ptr<Trigger::Response> & response)
+  {
+    gripper_interface_.setNamedTarget(target);
+    if (!gripper_interface_.move()) {
+      response->success = false;
+      response->message = failure_message;
+      return false;
+    }
+
+    response->success = true;
+    response->message = success_message;
+    return true;
+  }
+
+  bool move_arm_to_carry_pose(const std::shared_ptr<Trigger::Response> & response)
+  {
+    arm_interface_.setJointValueTarget(carry_pose);
+    if (!arm_interface_.move()) {
+      response->success = false;
+      response->message = "failed to move to carry pose";
+      return false;
+    }
+
+    response->success = true;
+    response->message = "moved to carry pose";
+    return true;
+  }
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+
+  auto node = std::make_shared<rclcpp::Node>(
+    "grasping",
+    rclcpp::NodeOptions().automatically_declare_parameters_from_overrides(true));
+
+  GraspingServices services(node);
+
+  rclcpp::executors::MultiThreadedExecutor executor;
+  executor.add_node(node);
+  executor.spin();
+  rclcpp::shutdown();
+  return 0;
+}
