fe6f2072afced198ebb85bd33566799415571776
Frank Zillmann <frank.zillmann@tum.de>
2026-06-11 09:03:00 +0200

    add pc.launch.py

--- tetris_assembly_bringup/launch/pc.launch.py
+++ tetris_assembly_bringup/launch/pc.launch.py
@@ -1,5 +1,20 @@
 from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import os
 
 
 def generate_launch_description():
-	return LaunchDescription([])
+    orchestrator_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('orchestration'),
+                'launch', 'orchestrator.launch.py'
+            )
+        )
+    )
+
+    return LaunchDescription([
+        orchestrator_launch,
+    ])
